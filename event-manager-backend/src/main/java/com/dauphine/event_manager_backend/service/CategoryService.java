378b8e934b046d4757559c683fd2bde10926c487
PepperMint5 <alice.boursin@dauphine.eu>
2024-06-12 17:29:10 +0200

    (Add) : category endpoints, service and repository implementation

---
+++ event-manager-backend/src/main/java/com/dauphine/event_manager_backend/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.dauphine.event_manager_backend.service;
+
+import com.dauphine.event_manager_backend.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    public List<Category> getAll();
+}
