70bc95709a50e7f08d89a88fd3e1ab32582a5400
PepperMint5 <alice.boursin@dauphine.eu>
2024-06-12 17:29:59 +0200

    (Add) : swagger documentation of EventManagerBackendApplication class

--- event-manager-backend/src/main/java/com/dauphine/event_manager_backend/EventManagerBackendApplication.java
+++ event-manager-backend/src/main/java/com/dauphine/event_manager_backend/EventManagerBackendApplication.java
@@ -1,9 +1,20 @@
 package com.dauphine.event_manager_backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Event manager backend",
+				description = "Even manager endpoints and apis",
+				contact = @Contact(name = "Alice Boursin", email = "alice.boursin@dauphine.eu"),
+				version = "1.0.0"
+		)
+)
 public class EventManagerBackendApplication {
 
 	public static void main(String[] args) {
