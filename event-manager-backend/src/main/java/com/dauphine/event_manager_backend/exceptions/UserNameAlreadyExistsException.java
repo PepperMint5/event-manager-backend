0694551133ccb22d6a5c0b07e86a7d747df6e90f
PepperMint5 <alice.boursin@dauphine.eu>
2024-06-22 23:03:02 +0200

    (Update) : text message from UserName LAreadyExist exception

--- event-manager-backend/src/main/java/com/dauphine/event_manager_backend/exceptions/UserNameAlreadyExistsException.java
+++ event-manager-backend/src/main/java/com/dauphine/event_manager_backend/exceptions/UserNameAlreadyExistsException.java
@@ -4,7 +4,7 @@ package com.dauphine.event_manager_backend.exceptions;
 public class UserNameAlreadyExistsException extends Exception {
 
     public UserNameAlreadyExistsException(String username) {
-        super("User with username " + username + " already exists.");
+        super("Username " + username + " is already taken.");
     }
 
 }
