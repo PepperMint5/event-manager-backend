4a40b74f3c1d4d0368399049b7c8dc8a39208922
PepperMint5 <alice.boursin@dauphine.eu>
2024-06-25 17:06:25 +0200

    (Add) : friends and account features (sorry forget to stage all before commit)

--- event-manager-backend/src/main/java/com/dauphine/event_manager_backend/exceptions/FriendshipAlreadyExistException.java
+++ event-manager-backend/src/main/java/com/dauphine/event_manager_backend/exceptions/FriendshipAlreadyExistException.java
@@ -1,2 +1,9 @@
-package com.dauphine.event_manager_backend.exceptions;public class FriendshipAlreadyExistException {
+package com.dauphine.event_manager_backend.exceptions;
+
+import com.dauphine.event_manager_backend.model.User;
+
+public class FriendshipAlreadyExistException extends Exception {
+    public FriendshipAlreadyExistException(User user){
+        super("Yout are already friend with " + user.getUsername() + ".");
+    }
 }
